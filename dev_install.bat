e48a30f3bfbee0fea19cb81044f3090170a366ca
Alex Janka <alex@alexjanka.com>
2022-07-20 14:50:35 +1000

    better dev_install

--- dev_install.bat
+++ dev_install.bat
@@ -2,4 +2,4 @@ call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build
 
 cmake -B build -DCMAKE_PREFIX_PATH=C:\JUCE -DCMAKE_BUILD_TYPE=Release
 cmake --build build --target Clipping_VST3
-copy ".\build\installer\Clipping.vst3" "..\dev-install\Clipping.vst3"
+copy ".\build\Clipping_artefacts\Release\VST3\Clipping.vst3\Contents\x86_64-win\Clipping.vst3" "..\dev-install\Clipping.vst3"
